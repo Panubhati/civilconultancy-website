3262361ddf814d7840854700f95c43b944f900a5
pranav bhatikare <pranavbhatikare7517@gmail.com>
2026-04-15 09:54:07 +0530

    fix: allow x-admin-key header in CORS

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(
             return callback(new Error("Not allowed by CORS"));
         },
         methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type"],
+        allowedHeaders: ["Content-Type", "x-admin-key"],
     })
 );
 
